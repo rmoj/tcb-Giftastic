ba2edb491a33af4fb08c58a11b40cafa83ead049
rmoj <rmoj99@gmail.com>
2018-07-13 21:09:32 -0400

    added getJson

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,49 @@
 'use strict';
 
 $(document).ready(function() {
-  $('#add').on('click', function() {
-    for (var i = 0; i < 3; i++) {
-      var btn = $('<button>');
-      btn.attr('id', i);
-      btn.text(i);
-      btn.on('click', function() {
-        console.log($(this).attr('id'));
+  $('#add').on('click', renderButton);
+  var i = 0;
+  function renderButton() {
+    var btn = $('<button>');
+    btn.attr('id', i);
+    btn.text(i);
+    btn.on('click', dummyFunction);
+    $('#button-holder').append(btn);
+    i++;
+  }
+
+  function getJson() {
+    var searchTerm = 'quokka';
+    var limit = 5;
+    var rating = 'G';
+    var queryURL =
+      'https://api.giphy.com/v1/gifs/search?api_key=' +
+      apiKey +
+      '&q=' +
+      searchTerm +
+      '&limit=10&offset=0&rating=' +
+      rating +
+      '&lang=en';
+    var apiKey = 'Xon9MP7X2uR0jVetMBZoD8fQeb5hPodw';
+
+    $.ajax({
+      url: queryURL,
+      method: 'GET'
+    })
+      .then(parseJson)
+      .catch(function(error) {
+        console.log(error);
       });
-      $('#button-holder').append(btn);
-    }
-  });
+  }
+
+  function dummyFunction() {
+    console.log($(this).attr('id'));
+  }
+
+  function parseJson(response) {
+    console.log(response);
+  }
+
+  var r = getJson();
+  console.log(r);
 });
